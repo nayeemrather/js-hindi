9836bbd4926190aeb853f3460f3b836101772475
nayeemrather <46628036+nayeemrather@users.noreply.github.com>
2022-12-29 06:26:47 +0000

    js Variables

---
+++ 02-Variables/variables.js
@@ -0,0 +1,10 @@
+var userName = "Rather"
+let accountEmail = "admskdn@gmail.com"
+const accountId = 23042
+//console.table([userName, accountEmail, accountId]);
+
+userName = "John"
+accountEmail = "jhbdh@gmail.com"
+accountAddress = "Gangbal"
+
+ console.table([userName,accountEmail,accountAddress]);
