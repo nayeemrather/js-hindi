118a93ef78c6ccff75ca0166d2664a31fc238faa
nayeemrather <46628036+nayeemrather@users.noreply.github.com>
2022-12-29 06:55:31 +0000

    datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,14 @@
+"use strict";  // treat all JS code as newer version
+
+// number => 2 to power 53
+//bigint
+// string => ""
+// null => standalone value
+// undefined
+// symbol => unique
+
+// object
+
+console.log(typeof "John");
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
